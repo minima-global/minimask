9ad3d9a7f85c525d18aaf5537302093f37c7ec2f
spartacusrex <patrickcerri@gmail.com>
2026-05-12 09:08:33 +0100

    small change

--- site/webwallet/js/variables.js
+++ site/webwallet/js/variables.js
@@ -2,6 +2,7 @@
 /**
  * MEG Variables
  */
+//var MINIMASK_HOST			="https://127.0.0.1:8080/wallet/";
 var MINIMASK_HOST			="https://minimask.org:8888/wallet/";
 var MINIMASK_MEG_USER		="apicaller";
 var MINIMASK_MEG_PASSWORD	="apicaller";
